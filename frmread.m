c6d43f789c799dd8ac6d8fc615801ef9622910af
wathcosmo <libuyu_cosmo@163.com>
2014-12-24 19:05:21 +0800

    update frmread

--- frmread.m
+++ frmread.m
@@ -1,7 +1,29 @@
 function imgout = frmread(videoID, num)
 %FRMREAD get specific frame
-    folderID = ceil(num / 960);
-    imgout = imread(strcat('pics/m', num2str(videoID), 'pics(', ...
+% imgout = frmread(videoID, num)
+% videoID =  1 : video01 frame, 
+%            2 : video02 blue frame, 
+%            3 : video02 gray frame
+    if videoID == 1
+        folderID = ceil(num / 960);
+        imgout = imread(strcat('pics/m', num2str(videoID), 'pics(', ...
         num2str(folderID), ')/im', num2str(num), '.jpg'));
+    elseif videoID == 2
+        if num > 17000
+            folderID = 18;
+        else
+            folderID = ceil(num/1000);
+        end
+        imgout = imread(strcat('pics/m2blue(', num2str(folderID), ...
+            ')/im', num2str(num), '.jpg'));
+    else
+        if num > 17000
+            folderID = 18;
+        else
+            folderID = ceil(num/1000);
+        end
+        imgout = imread(strcat('pics/m2gray(', num2str(folderID), ...
+            ')/im', num2str(num), '.jpg'));
+    end
 end
 
