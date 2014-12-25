8ad9b25e2c9b4a9d9ab157a050592ed5f8c8777b
wathcosmo <libuyu_cosmo@163.com>
2014-12-25 14:29:22 +0800

    rename work.m as run.m

--- getVib.m
+++ getVib.m
@@ -1,25 +1,43 @@
 function vibwav = getVib(videoID, frms)
 %GETVIB get vibration wave
 %   vibwav = getVib(videoID, frms)
+    addpath('./mexopencv-master');
 
     n = length(frms);
     %[img, bg] = denoised(videoID, frms(1));
     img = frmread(videoID, frms(1));
-    tmpl = img(900:1050, 800:1000);
+    
     X = zeros(n, 2);
-    X(1, :) = [900, 800];
+    if videoID == 1
+        %tmpl = img(80:280, 800:1200);
+        tmpl = img(950:1060, 900:1000);
+        X(1, :) = [51, 51];
+    else
+        tmpl = img(200:800, 1650:1850);
+        X(1, :) = [101, 101];
+    end
+    
+    
     
     for k = 2 : n
         %im = denoised(videoID, frms(k), bg);
         im = frmread(videoID, frms(k));
-        dif = cv.matchTemplate(im(800:1080, 700:1100), tmpl);
-        [r,c] = find(dif == min(dif(:)));
-        X(k, :) = [799+r, 699+c];
+        if videoID == 1
+            dif = cv.matchTemplate(im(900:1080, 850:1050), tmpl);
+            %dif = cv.matchTemplate(im(30:330, 750:1250), tmpl);
+            [r,c] = find(dif == min(dif(:)), 1);
+            X(k, :) = [r, c];
+        else
+            dif = cv.matchTemplate(im(100:900, 1550:1920), tmpl);
+            [r,c] = find(dif == min(dif(:)), 1);
+            X(k, :) = [r, c];
+        end
     end
     
-    avg = mean(X);
-    X = X - repmat(avg, n, 1);
+    X(:,1) = detrend(X(:,1));
+    X(:,2) = detrend(X(:,2));
     [~, ~, v] = svd(X);
+    %[v, ~] = eigs(X'*X, 1);
     vibwav = X * v;
 end
 
---
+++ run.m
@@ -0,0 +1,76 @@
+%% addpath
+% mexopencv
+% addpath('./mexopencv-master');
+% mmread
+% addpath('./mmread');
+% video = mmread('01.MTS', 1, [], false, true);
+% img = video.frames(1).cdata;
+% % textureSynth
+% addpath('./textureSynth/matlabPyrTools/');
+% addpath('./textureSynth/textureSynth/');
+% addpath('./textureSynth/colorTextureSynth/');
+% addpath('./textureSynth/matlabPyrTools/MEX');
+
+%%
+%distractPics('02.MTS', '02', 1:18314);
+
+%% video01
+% time mapping
+
+% syn 200Hz
+% about 1 sec = 1 : 780
+
+% single freq
+% 3 sec = 1741 : 2700
+% 4 sec = 2701 : 3660
+% 5 sec = 3661 : 4620
+
+% double freq
+% 7 sec = 5561 : 6520
+% 8 sec = 6561 : 7520
+% 9 sec = 7521 : 8480
+
+% music
+% 11~17 sec = 9441 : 16160
+
+tic;
+
+vibwav = getVib(1, 7521 : 8480);
+plot(vibwav)
+
+freq = abs(fft(vibwav));
+figure
+plot(freq)
+
+toc;
+
+
+%% video02
+% time mapping
+
+% syn 200Hz = 1 : 660
+
+% single freq
+% 3 sec = 1621 : 2580
+% 4 sec = 2581 : 3540
+% 5 sec = 3541 : 4500
+
+% double freq
+% 7 sec = 5461 : 6420
+% 8 sec = 6421 : 7380
+% 9 sec = 7381 : 8340
+
+% music
+% 11~17 sec = 9280 : 16000
+
+% tic
+
+% vibwav = getVib(2, 1:18000);
+% figure;
+% plot(vibwav);
+% 
+% freq = abs(fft(vibwav));
+% figure;
+% plot(freq);
+
+% toc
